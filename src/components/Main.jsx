dd2bebd6cfe691f853b2e89e0df724a134f15ae6
Jonathan <jonathanestebangalan@gmail.com>
2021-09-02 18:13:16 +0200

    Diseño cuestionable

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,12 +4,12 @@ import Form from './Form'
 
 const Main = () => {
   return (
-    <main className="main">
+    <div className="main">
       <h1 className="main__title">Elige Planet Express para tus envíos Espaciales</h1>
       <p className="main__text">"¡Nuestro equipo es reemplazable, su paquete no lo es!"</p>
       <p className="main__text">Suscríbete para recibir notificaciones de ¡Grandes ofertas!</p>
       <Form />
-    </main>
+    </div>
   )
 }
 
