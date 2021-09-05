b5154a4cfec6fafc58f44c8805e55188dcfcfac1
Jonathan <jonathanestebangalan@gmail.com>
2021-09-05 11:30:28 +0200

    Mejoras del diseño en mobile

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
+import warning from '../assets/advertencia.png'
 import "../styles/Form.css";
 
 const Form = () => {
   const [text, setText] = useState("");
+  const [isSuscribed, setIsSuscribed] = useState(false)
+  const [isValidator, setIsValidator] = useState(false)
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const validator =  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     if (text.trim() === "") {
       alert("Los campos están vacíos");
       setText("");
+    } else if(validator.test(text)){
+      alert(`¡Te has suscrito, Enhorabuena!
+      ${text}`);
+      setIsSuscribed(true)
+      setIsValidator(false)
     } else {
-      alert(text);
+      setIsValidator(true)
     }
     setText("");
   };
@@ -25,7 +34,8 @@ const Form = () => {
           value={text}
           onChange={(event) => setText(event.target.value)}
         />
-        <input className="form__submit" type="submit" value="Suscríbete" />
+        <input className="form__submit" type="submit" value={isSuscribed ? 'Suscrito' : 'Suscríbete'} />
+        {isValidator && <p className="validate"><span><img className="warning" src={warning} alt="" /></span> El email está mal escrito</p>}
       </form>
     </>
   );
